7baf4f6c623553d8ebf2dae2a6d9616163507896
niveus323 <niveus323@gmail.com>
2022-01-06 15:58:17 +0900

    프로젝트 생성

---
+++ src/main/java/com/sectionplayer/SectionplayerApplication.java
@@ -0,0 +1,13 @@
+package com.sectionplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectionplayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SectionplayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sectionplayer/SectionplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sectionplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectionplayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
